a83207282599832f0e401ffbb12a9e4c767b6f74
Sirrobindoger <robindoger@gmail.com>
2021-05-09 17:00:01 -0400

    Creating fumo thing

--- src/main/java/org/sprojects/smod/ModItems.java
+++ src/main/java/org/sprojects/smod/ModItems.java
@@ -4,16 +4,24 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.sprojects.smod.blocks.FumoBlock;
+import org.sprojects.smod.blocks.MarisadBlock;
 import org.sprojects.smod.items.Marisad;
 
 public class ModItems {
     @GameRegistry.ObjectHolder("smod:marisad")
     public static Marisad marisad;
 
+    @GameRegistry.ObjectHolder("smod:marisadblock")
+    public static MarisadBlock marisadBlock;
+
+    @GameRegistry.ObjectHolder("smod:fumoblock")
+    public static FumoBlock fumoBlock;
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         marisad.initModel();
+        marisadBlock.initModel();
+        fumoBlock.initModel();
     }
 
 }
